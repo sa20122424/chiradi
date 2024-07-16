675dd88de95d5f971e2024d45f315863d33d9684
sa20122424 <saloxiddin201224@gmail.com>
2024-07-16 11:17:35 +0500

    15 coin поставте пожалуста у меня 5 раз chiradi miya

---
+++ main.js
@@ -0,0 +1,18 @@
+const userName = prompt('string tipidiga maulumot kiriting' )
+const userAge = +prompt('Number tipidiga maulumot kiriting' )
+const userBuling = prompt('boolin tipidiga maulumot kiriting' )
+ 
+
+
+alert(`
+   Siz kiritgan ${userName} sozi ${typeof userName } tiypiga tegishli
+   Siz kiritgan ${userAge} sozi ${typeof Number(userAge) } tiypiga tegishli
+   Siz kiritgan ${userBuling} sozi ${typeof Boolean(userBuling) } tiypiga tegishli
+`);
+console.log(` 
+    Siz kiritgan ${userName} sozi ${typeof userName } tiypiga tegishli
+    Siz kiritgan ${userAge} sozi ${typeof Number(userAge) } tiypiga tegishli
+    Siz kiritgan ${userBuling} sozi ${typeof Boolean(userBuling) } tiypiga tegishli
+`);
+   
+ 
